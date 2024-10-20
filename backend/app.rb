b11d0614be04401d68255d65f9e52d58fb611688
jkRoberts <jacob.roberts.nz@gmail.com>
2024-10-20 21:29:18 +1300

    Added new EmployeeResource
    Added new filters and attributes for querying

--- backend/app.rb
+++ backend/app.rb
@@ -5,6 +5,7 @@ require 'kaminari'
 require 'sinatra/base'
 require 'graphiti'
 require 'graphiti/adapters/active_record'
+require 'pry'
 
 class ApplicationResource < Graphiti::Resource
   self.abstract_class = true
@@ -21,7 +22,35 @@ class DepartmentResource < ApplicationResource
 
   attribute :name, :string
 
-  # has_many :employees
+  has_many :employees
+end
+
+class EmployeeResource < ApplicationResource
+  self.model = Employee
+  self.type = :employees
+
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :age, :integer
+  attribute :position, :string
+  attribute :department_name, :string do
+    @object.department.name
+  end
+  attribute :name, :string, readable: false do
+    "#{@object.first_name} #{@object.last_name}"
+  end
+
+  filter :name, single: true do
+    fuzzy_match do |scope, value|
+      scope.where('first_name LIKE ? OR last_name LIKE ?', "%#{value}%", "%#{value}%")
+    end
+  end
+
+  filter :department_name, single: true do
+    eq do |scope, value|
+      scope.joins(:department).where(department: {name: value})
+    end
+  end
 end
 
 Graphiti.setup!
@@ -37,6 +66,18 @@ class EmployeeDirectoryApp < Sinatra::Application
 
   after do
     ActiveRecord::Base.connection_handler.clear_active_connections!
+  end
+
+
+  # to paginate - &page[size]=20&page[number]=2
+  # to filter - &filter[department_name][eq]=IT
+  # to fuzzy filter - &filter[name][fuzzy_match]=IT
+  # to sort - &sort=first_name (ASC) || &sort=-first_name (DESC)
+  # total count - &stats[total]=count
+
+  get '/api/v1/employees' do
+    employees = EmployeeResource.all(params)
+    employees.to_jsonapi
   end
 
   get '/api/v1/departments' do
