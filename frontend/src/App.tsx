d04f401c0f6ca357330f902184a69b9389a49b78
jkRoberts <jacob.roberts.nz@gmail.com>
2024-10-20 21:29:35 +1300

    FE WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
+import { useReactTable } from '@tanstack/react-table'
+import { useState } from 'react';
 
 const App = () => {
+  const [sort, setSort] = useState(
+    {
+      column: '',
+      sortDirection: 'ASC'
+    }
+  )
+
+  const data = {
+
+  }
+
   return (
     <>
       <h1 className="text-2xl font-bold underline">Keyhook Test Project</h1>
